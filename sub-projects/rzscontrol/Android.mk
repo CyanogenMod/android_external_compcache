5fdea2182e615432f615971d9353734fabf5ade0
Tom Giordano <tomgiordano83@gmail.com>
2011-04-07 19:25:09 +1000

    compcache: Add LOCAL_MODULE_TAGS
    Change-Id: I1bba626ede55279a4424833aa1c6a91ebf7a144a

--- sub-projects/rzscontrol/Android.mk
+++ sub-projects/rzscontrol/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := rzscontrol.c
 LOCAL_C_INCLUDES := external/compcache/sub-projects/include external/compcache
 
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := rzscontrol
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 
